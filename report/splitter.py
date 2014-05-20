3348ea74abdf1faae546a61f2feb75a545099ab4
daemontus <daemontus@gmail.com>
2014-05-20 18:11:33 +0200

    Demo na grafy

---
+++ report/splitter.py
@@ -0,0 +1,46 @@
+from random import randint
+import sys
+
+f = open(sys.argv[1])	
+
+head = True
+entries = 30
+values = [ [] for x in range(entries) ]
+labels = []
+for line in f:
+	if head:
+		head = False
+	else:
+		split = line.split()
+		labels.append(split[0])
+		split = split[1:]
+		split = map(float, split)
+		for x in range(entries):
+			values[x].append(split[x])
+
+for x in range(len(labels)):
+	if x%2 == 0:
+		labels[x] = ''
+
+print "var lineChartData = {"
+print "	labels :", labels,","
+print "	datasets : ["
+first = True
+
+colors = [
+	(139,0,139),
+]
+
+for entry in values:
+	if not first:
+		print "		,"
+	else:
+		first = False
+	print "		{"
+	print "			strokeColor : \"rgba(%d,%d,%d,1)\"," % (randint(0,255), randint(0,255), randint(0,255))
+	print "			fillColor : \"rgba(220,220,220,0)\","
+	print "			pointStrokeColor : \"#fff\","
+	print "			data : ", entry, ","
+	print "		}"
+print "	]"
+print "}"
